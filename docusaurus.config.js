7027285d78305372517512e77063fc27fe5ff480
Tan Jun Rong <JRtan1191dev@gmail.com>
2023-06-19 10:57:40 +0800

    Updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,8 @@ const config = {
   organizationName: 'sgtechstack', // Usually your GitHub org/user name.
   projectName: 'contribution-tutorial', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
